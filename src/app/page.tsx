99f95be864609fcb332ecf0ea3f617d1f70c84bf
David Muriithi <88942514+BarriosXJavier@users.noreply.github.com>
2024-09-21 08:36:29 +0000

    modified:   bun.lockb 	modified:   data/index.ts 	modified:   src/app/page.tsx 	new file:   src/components/ui/3d-pin.tsx 	modified:   src/components/ui/floating-nav.tsx 	modified:   src/components/ui/shared/recent-projects.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,14 @@ import { FloatingNav } from "@/components/ui/floating-nav";
 import { HomeIcon } from "lucide-react";
 import Grid from "@/components/ui/shared/grid";
 import RecentProjects from "@/components/ui/shared/recent-projects";
+import { navItems } from "../../data";
 
 
 export default function Home () {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto  sm:px-10 px-5">
-      <FloatingNav navItems={[{
-        name: "Home",
-        link: "/",
-        icon: <HomeIcon />
-      }]}/>
+      <FloatingNav navItems={navItems}/>
+
       <div className="max-w-7xl w-full">
         <HeroSection />
         <Grid />
