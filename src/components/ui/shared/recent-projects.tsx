b507bd8254b3b2d89f7dd6b99f0585a4bbda48ed
David Muriithi <muriithid05@gmail.com>
2024-10-16 18:17:09 +0300

    modified:   src/components/ui/shared/recent-projects.tsx

--- src/components/ui/shared/recent-projects.tsx
+++ src/components/ui/shared/recent-projects.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { projects } from "../../../../data";
+import { TypewriterEffect } from "@/components/ui/typewriter-effect";
 
 type Props = {};
 
 const RecentProjects = (props: Props) => {
   return (
-    <div className="py-18" id="projects">
-      <h1 className="heading text-purple text-center">
-        recent projects
-      </h1>
+    <div className="py-10" id="projects">
+      <TypewriterEffect
+        words={[{ text: "recent" }, {text: " "},  {text: "projects"}]}
+        className="heading text-purple text-center"
+      />
       <div className="p-4 mt-10">
         <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
           {projects.map(({ id, title, des, img, iconLists, link }) => (
@@ -28,7 +30,7 @@ const RecentProjects = (props: Props) => {
                 <img
                   src={img}
                   alt={title}
-                  className="z-10 absolute bottom-0 max-h-[20vh] mb-2"
+                  className="z-10 absolute inset-0 object-contain w-full h-full"
                 />
                 <div className="z-20 relative text-center mt-2">
                   <h1 className="font-bold lg:text-2xl md:text-xl text-base">
@@ -54,7 +56,7 @@ const RecentProjects = (props: Props) => {
                   ))}
                 </div>
 
-                <div className="flex justify-center items-center bg-red-400 relative px-3 py-2 mt-6 rounded-md">
+                <div className="flex justify-center items-center bg-gray-700 relative px-3 py-2 mt-6 rounded-md">
                   <p className="lg:text-xl md:text-xs text-sm text-white">
                     check live site
                   </p>
